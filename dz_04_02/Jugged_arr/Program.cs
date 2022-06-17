72e2d224cdbfdc323ee7cbe48e12676dca5aacdd
Relrige <91017454+Relrige@users.noreply.github.com>
2022-06-17 19:00:13 +0300

    Add files via upload

---
+++ dz_04_02/Jugged_arr/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace DZ_04_01
+{
+    class Program
+    {
+        static int[][] CreateJuggedArr(params int[] cols)
+        {
+            int[][] m = new int[cols.Length][];
+
+            for (int i = 0; i < m.Length; i++)
+            {
+                m[i] = new int[cols[i]];
+            }
+
+            return m;
+        }
+
+        static void FillRand(int[][] matrix, int left, int right)
+        {
+            Random rand = new Random();
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    matrix[i][j] = rand.Next(left, right + 1);
+                }
+            }
+        }
+
+        static void PrintJugged(int[][] matrix)
+        {
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    Console.Write($"{matrix[i][j],10}\t");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Up(ref int[][] matrix, int indexOfRow, int count)
+        {
+            //if (indexOfRow != 0)
+            //{
+            //if (indexOfRow - count >-1)
+            //{
+            for (int i = indexOfRow; i > indexOfRow - count; i--)
+            {
+                SwapRows(matrix, i, i - 1);
+            }
+            //  }
+            //}
+        }
+        static void SwapRows<T>(T[][] m, int row1, int row2)
+        {
+            if (!IsValidRow(row1) || !IsValidRow(row2))
+            {
+                return;
+            }
+            T[] tmp = m[row1]; // свопаємо посилання на рядки 
+            m[row1] = m[row2];
+            m[row2] = tmp;
+
+            bool IsValidRow(int r) => r >= 0 && r < m.Length;
+        }
+        static void AddRow(ref int[][] matrix, int index, int[] newRow)
+        {
+            Array.Resize(ref matrix, matrix.Length + 1);
+            matrix[matrix.Length - 1] = newRow;
+            SwapRows(matrix, matrix.Length - 1, index);
+            for (int i = matrix.Length - 1; i > index + 1; i--)
+            {
+                SwapRows(matrix, i, i - 1);
+            }
+        }
+        static void Remove(ref int[][] matrix, int index)
+        {
+            int[][] tmp = new int[matrix.Length - 1][];
+
+            int j = 0;
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                if (i == index)
+                {
+                    continue;
+                }
+                else
+                {
+                    tmp[j] = matrix[i];
+                    j++;
+                }
+            }
+
+            Array.Resize(ref matrix, matrix.Length - 1);
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                matrix[i] = tmp[i];
+            }
+        }
+        static void Down(ref int[][] matrix, int indexOfRow, int count)
+        {
+            if (indexOfRow != matrix.Length)
+            {
+                if (indexOfRow + count < matrix.Length)
+                {
+                    for (int i = indexOfRow; i < indexOfRow + count; i++)
+                    {
+                        SwapRows(matrix, i, i + 1);
+                        //SwapRows(matrix, i, i - 1);
+                    }
+                }
+            }
+        }
+
+        static StringBuilder JuggedToStrB(int[][] matrix)
+        {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    builder.Append(matrix[i][j]+" ");
+                }
+            }
+            Console.Write(builder);
+            return builder;
+        }
+        static void Main()
+        {
+            int[][] matrix = CreateJuggedArr(2, 3, 6, 5, 4);
+            FillRand(matrix, 1, 100);
+
+            //PrintJugged(matrix);
+            //Console.WriteLine();
+            //int[] arr = { 1, 2, 3, 4, 5 };
+            //AddRow(ref matrix, 1, arr);
+            ////Remove(ref matrix, 1);
+            //PrintJugged(matrix);
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Down(ref matrix, 0, 2);
+            //PrintJugged(matrix);
+
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Up(ref matrix, 3, 2);
+            //PrintJugged(matrix);
+            JuggedToStrB(matrix);
+        }
+    }
+}
---
+++ dz_04_02/dz_04_02/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace dz_04_02
+{
+    class Program
+    {
+        static void RemoveAllCharFromString(ref string str, params char[] charsToRemove)
+        {
+            foreach (var c in charsToRemove)
+            {
+                str = str.Replace(c, '\0');
+            }
+        }
+        static void printJugger(char[][] jug)
+        {
+            foreach (var arr in jug)
+            {
+
+                Console.Write($"{arr[0]} [{arr.Length}]");
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
+        }
+        static void CountLetterInString(string str)
+        {
+            char[] letter = { 'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i','J','j','K','k','L','l','M','m','N','n','O','o', 'P','p','Q','q','R','r','S','s','T','t','U','u','V','v','W','w','X','x','Y','y','Z','z' };
+            char[][] countLetter = new char[0][];
+            int index = 0;
+            foreach (var item in letter)
+            {
+                int count = str.Count((e) => e.Equals(item));
+                if (count > 0)
+                {
+                    Array.Resize(ref countLetter, countLetter.Length + 1);
+                    Array.Resize(ref countLetter[index], count);
+                    countLetter[index][0] = item;
+                    ++index;
+                }
+            }
+            printJugger(countLetter);
+        }
+        static bool check(string str)
+        {
+            char a = str[0];
+            int length = str.Length;
+            bool bb =str.All(str => a!='_');
+            bool bb1 =str.All( str =>!Char.IsDigit(a) );
+            bool bb2 = str.All(str =>length<256);
+            if (bb == true && bb1 == true && bb2 == true)
+            {
+                return true;
+            }
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            //string str = "abcdbxad lofdfad";
+            //char[] charToDelete = { 'a', 'f','b' };
+            //RemoveAllCharFromString(ref str, charToDelete);
+            //Console.WriteLine(str);
+
+            //string str2 = "I don’t know whether to delete this or rewrite it";
+            //CountLetterInString(str2);
+
+            ////
+            string str1 = "Afsfsf";
+            bool b =check(str1);
+            Console.WriteLine(b);
+
+            string str2 = "2Afsfsf";
+            bool bc = check(str2);
+            Console.WriteLine(bc);
+        }
+    }
+}
